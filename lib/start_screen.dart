5fa3113984384985a63024585df65b280671096b
masamichan <positivemasa07@gmail.com>
2025-05-27 11:02:54 +0900

    add Button component-very tired

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -8,7 +8,23 @@ class StartScreen extends StatelessWidget {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [Image.asset('assets/images/quiz-logo.png', width: 300)],
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: 300),
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way',
+            style: TextStyle(
+              color: Color.fromARGB(255, 226, 222, 228),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
